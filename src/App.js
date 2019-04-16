adc96dbd918f7338151cd2b8e498679e0440d92a
keng.crp <powerkangza@msn.com>
2019-04-17 02:55:44 +0700

    two commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Side from './side.js';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Side/>
+      </div>
+    );
+  }
+}
+
+export default App;
